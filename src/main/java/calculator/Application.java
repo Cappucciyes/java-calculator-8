e19c6c5d232c2e0bcd7d5316752561dac1e030d5
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2025-10-19 01:14:45 +0900

    feat: getSum 함수 추가
    사용자가 올바르게 입력한다고 가정하고, getSum을 활용하여 기본 구분자 (',', ':')를 기준으로 최종 합을 구하는 기능 추가.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,11 +2,26 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
 
-        System.out.println(String.format("결과 : %s",userInput));
+        String delimiter = ":,";
+        int result = getSum(delimiter, userInput);
+        System.out.println(String.format("결과 : %d",result));
+        Console.close();
+    }
+
+    private static int getSum(String delimiter, String inputBuff) {
+        int result = 0;
+        StringTokenizer tokens = new StringTokenizer(inputBuff, delimiter);
+        while(tokens.hasMoreTokens()) {
+            result += Integer.parseInt(tokens.nextToken());
+        }
+
+        return result;
     }
 }
